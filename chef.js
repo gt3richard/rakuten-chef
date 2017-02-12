e644950b6b2499c8a9f321ba04fd9b54de0ab158
gt3richard <gt3richard@gmail.com>
2017-02-11 20:39:31 -0800

    Add screen scraping logic, ugh not pretty

--- chef.js
+++ chef.js
@@ -1,49 +1,201 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var requester = require('request');
+var jsonQuery = require('json-query');
+var cheerio = require('cheerio');
 var app = express();
-
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
+
+function NormalizeDay(date)
+{
+	var day = 0;
+	if(date == 'today')
+	{
+		day = new Date().getDay();
+	}
+	else if(date == 'tomorrow')
+	{
+		var currDay = new Date();
+		day = currDay.setDate(currDay.getDate() + 1).getDay();
+	}
+	
+	switch (day) {
+    case 0: return "Sunday";
+    case 1: return "Monday";
+	case 2: return "Tuesday";
+	case 3: return "Wednesday";
+	case 4: return "Thursday";
+	case 5: return "Friday";
+	case 6: return "Saturday";
+	}
+}
+
 
 app.get('/', function(request, response) {
   response.send("Hi, I'm Rakuten Chef!");
 });
 
-app.post('/', function(request, response) {
+app.get('/json', function(request, response){
+	var sourceUrl = "http://static.wixstatic.com/sites/068198_cef0516e50e786d502f2ef867a85196e_572.json.z?v=3";
+
+	requester({method: 'GET', uri: sourceUrl , gzip: true, json: true} ,function (error, res, body) {
+		if (!error && res.statusCode == 200) {
+			
+			var html = jsonQuery('data.document_data.dataItem-iqtw5gn6.text', { data: body}).value;
+			var $ = cheerio.load(html);
+			
+			$('p').each(function(i, elem) {
+				var value = $(this).text().replace(/[^a-z0-9\s]/gi, ' ').replace('entree', '').replace('main', '').replace('soups','').replace('sides','').replace('breakfast','').trim();
+				
+				if(value.length > 0)
+				{
+					$(this).addClass('needparse');
+					$(this).addClass('food');
+				}
+			});
+			
+			$('h4').each(function(i, elem) {
+				if($(this).text().replace(/[\n\r\t]/g, "").trim())
+				{
+					$(this).addClass('needparse');
+					$(this).addClass('date');
+				}
+			});
+			
+			$('h5').each(function(i, elem) {
+				if($(this).text().replace(/[\n\r\t]/g, "").trim())
+				{
+					$(this).addClass('needparse');
+					
+					if($(this).text().trim().startsWith("Breakfast") || $(this).text().trim().startsWith("Lunch"))
+					{
+						$(this).addClass('meal');
+					}
+				}
+			});
+			
+			var activeMeal = 0;
+			var isFirst = 1;
+			var payload = [];
+			var item = {date:'',menu:{breakfast:[],lunch:[]}};
+			
+			$('.needparse').each(function(i, elem) {
+				if($(this).hasClass('meal'))
+				{
+					var value = $(this).text().replace(/[\n\r\t]/g, "");
+					
+					if(value.trim().startsWith('Breakfast'))
+					{
+						activeMeal = 0;
+					}
+					else if(value.trim().startsWith('Lunch'))
+					{
+						activeMeal = 1;
+					}
+					
+					//console.log("==============MEAL============ " + value);
+				}
+				else if($(this).hasClass('date'))
+				{
+					var value = $(this).text().replace(/[\n\r\t]/g, "");
+			
+					if(isFirst == 0)
+					{
+						payload.push(item);
+						item = {date:'',menu:{breakfast:[],lunch:[]}};
+						
+					}			
+					isFirst = 0;
+					item.date = value;
+
+					//console.log("###############DATE###############" + value);
+				}
+				else
+				{
+					var value = $(this).text().replace(/[\n\r\t]/g, ' ').trim().replace(/[^a-z0-9\s]/gi, ' ').replace('entree', '').replace('main', '').replace('soups','').replace('sides','').replace('breakfast','').trim();
+					
+					if(value.length > 0)
+					{
+						if(activeMeal == 0)
+						{
+							item.menu.breakfast.push(value);
+						}
+						else if(activeMeal == 1)
+						{
+							item.menu.lunch.push(value);
+						}
+						//console.log("Food " + value);
+					}
+				}
+			});
+			payload.push(item);
+			
+			return response.send(payload);
+		}
 	
-	console.log(request.body);
+		return response.send("Whoops!");
+	});
+});
+
+app.get('/tester', function(request, response) {
+  var date = new Date();
+  date.setDate(date.getDate() + 1);
+  
+  response.send(date.getDay().toString());
+    
+});
+
+app.post('/', function(request, response) {
 	
 	var date = request.body.result.parameters.date;
 	var meal = request.body.result.parameters.meal;
 	
-	var payload = [
-	{date:'2-13-2017',menu:{breakfast:['eggs','bacon'],lunch:['sushi']}},
-	{date:'2-14-2017',menu:{breakfast:['muffins'],lunch:['tacos','chips']}},
-	{date:'2-15-2017',menu:{breakfast:['bagels','yogurt'],lunch:['pizza']}}
-	];
+	if(date == "today" || date == "tomorrow")
+		date = NormalizeDay(date);
 	
-	var value = "We are having Soylent, sorry.";
-	if(date == 'today')
-	{
-		if(meal == 'breakfast')
-			value = "We are having "+ payload[0].menu.breakfast[0] +" today.";
-		else if(meal == 'lunch')
-			value = "We are having "+ payload[0].menu.lunch[0] +" today.";
-	}
-	
-	var apiResponse = {
-		speech: value,
-		displayText: value,
-		data: {},
-		contextOut: [],
-		source: 'rakuten-chef'
+	var myCallback = function(data) {
+		var value = "We are having Soylent, sorry.";
+		if(date == 'Saturday' || date == 'Sunday')
+		{
+			value = "It's the weekend. Cook your own food.";
+		}
+		else
+		{
+			for(var i = 0; i < data.length; i++)
+			{
+				if(data[i].date.startsWith(date))
+				{
+					if(meal == 'breakfast')
+						value = "We are having "+ data[i].menu.breakfast[0] +" on " + date + ".";
+					else if(meal == 'lunch')
+						value = "We are having "+ data[i].menu.lunch[0] +" on " + date + ".";
+				}
+			}
+		}
+		
+		var apiResponse = {
+			speech: value,
+			displayText: value,
+			data: {},
+			contextOut: [],
+			source: 'rakuten-chef'
+		};
+		
+		response.send(apiResponse);
 	};
 	
-	response.send(apiResponse);
+	requester({method: 'GET', uri: 'http://localhost:5000/json', json: true},function (error, res, body) {
+		if (!error && res.statusCode == 200) {
+			myCallback(body);
+		}
+	});
 });
 
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+	
